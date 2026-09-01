b5760ab5fbee394a758e3911215cad1dcf95f5db
helio enrique <jhelioenrique@gmail.com>
2026-09-01 11:10:02 -0300

    concerto do card de escrever

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Header } from '../../componentes/header/header';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideEdit2 } from '@ng-icons/lucide';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, Header],
+  imports: [RouterLink, Header, NgIcon, FormsModule],
+  providers: [
+    provideIcons({
+      lucideEdit2,
+    })
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home { }
+export class Home {
+
+  mensagem: string = '';
+
+  async onSubmit() {
+    console.log('Botão Enviar clicado');
+  }
+}
